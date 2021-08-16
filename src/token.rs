2e4acb18a51e5eb04eda7b5318b000e512d556b7
William Oldham <wegg7250@gmail.com>
2021-08-16 20:12:54 +0100

    Add debug

--- src/token.rs
+++ src/token.rs
@@ -2,11 +2,13 @@ mod selector;
 mod operator;
 mod modifier;
 
+#[derive(Debug)]
 pub(crate) struct Dice {
     number_of_dice: u8,
     number_of_sides: u8
 }
 
+#[derive(Debug)]
 pub(crate) enum Token {
     Selector(selector::SelectorType),
     // Operator, Value
--- src/token/selector.rs
+++ src/token/selector.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub(crate) enum SelectorType {
     GreaterThan,
     LessThan,
